c1875328483bfa0610bce89d46a50813acc873a0
Sraddha Bhattacharjee <sraddha.bhattacharjee@gmail.com>
2024-02-14 14:52:26 -0500

    #9 renamed record

--- src/main/java/de/dennisguse/opentracks/services/RecordingStatus.java
+++ src/main/java/de/dennisguse/opentracks/services/RecordingStatus.java
@@ -7,7 +7,7 @@ import de.dennisguse.opentracks.data.models.Track;
 
 public record RecordingStatus(@Nullable Track.Id trackId) {
 
-    static RecordingStatus record(@NonNull Track.Id trackId) {
+    static RecordingStatus recordCreate1(@NonNull Track.Id trackId) {
         return new RecordingStatus(trackId);
     }
 
@@ -24,4 +24,3 @@ public record RecordingStatus(@Nullable Track.Id trackId) {
     }
 }
 
-////
